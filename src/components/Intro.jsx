26e4aa4bfc9861458da220120aecdcd6eb47e1bd
Can <63351166+fastuptime@users.noreply.github.com>
2024-09-01 21:30:06 +0300

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -49,7 +49,7 @@ export function Intro() {
         </Link>
       </div>
       <h1 className="mt-14 font-display text-4xl/tight font-light text-white">
-        SpeedSMMM{' '}
+        SpeedSMM {' '}
         <span className="text-sky-300">Changelog</span>
       </h1>
       <p className="mt-4 text-sm/6 text-gray-300">
